e3e1d1454818a38f5f403b6ff4ef9bbd09608174
Pedro Alves <pedroalves.rj@icloud.com>
2026-03-25 17:40:29 -0300

    Criação da entidade Categoria e enum de Finalidade.

---
+++ ControleGastos/ControleGastos.Api/Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleGastos.Api.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "A Descrição é obrigatória.")]
+        [MaxLength(400, ErrorMessage = "A descrição deve ter no máximo 400 caracteres.")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "A Finalidade é obrigatória.")]
+        public TipoFinalidade Finalidade { get; set; }
+    }
+}
---
+++ ControleGastos/ControleGastos.Api/Models/TipoFinalidade.cs
@@ -0,0 +1,9 @@
+﻿namespace ControleGastos.Api.Models
+{
+    public enum TipoFinalidade
+    {
+        Despesa = 1,
+        Receita = 2,
+        Ambas = 3
+    }
+}
