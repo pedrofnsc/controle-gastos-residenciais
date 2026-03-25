2cf2325272e9396864ec30708ef99b18f6c5e2df
Pedro Alves <pedroalves.rj@icloud.com>
2026-03-25 20:30:36 -0300

    Configuração do EntityFramework, SQLite e primeira migration.

---
+++ ControleGastos/ControleGastos.Api/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ControleGastos.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleGastos.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Transacao> Transacoes { get; set; }
+    }
+}
