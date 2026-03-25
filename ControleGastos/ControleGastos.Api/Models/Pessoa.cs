a475105ff53e096a8c7ba8c20cd3a92cf161d808
Pedro Alves <pedroalves.rj@icloud.com>
2026-03-25 17:23:52 -0300

    Criação da entidade Pessoa

---
+++ ControleGastos/ControleGastos.Api/Models/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleGastos.Api.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O Nome é obrigatório.")]
+        [MaxLength(200, ErrorMessage = "O nome deve ter no máximo 200 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "A Idade é obrigatória.")]
+        public int Idade { get; set; }
+    }
+}
