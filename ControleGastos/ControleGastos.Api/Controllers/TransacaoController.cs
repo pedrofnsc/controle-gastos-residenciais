21318a2f879f2f8955efb9b031bd729de5e7be84
Pedro Alves <pedroalves.rj@icloud.com>
2026-03-26 13:12:04 -0300

    Criação de TransacaoController com GET e POST

---
+++ ControleGastos/ControleGastos.Api/Controllers/TransacaoController.cs
@@ -0,0 +1,26 @@
+﻿using ControleGastos.Api.Data;
+using ControleGastos.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleGastos.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransacaoController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Transacao>>> GetTransacoes()
+        {
+            return await context.Transacoes.Include(t => t.Pessoa).Include(t => t.Categoria).ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Transacao>> PostTransacao(Transacao transacao)
+        {
+            context.Transacoes.Add(transacao);
+            await context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTransacoes), new { id = transacao.Id }, transacao);
+        }
+    }
+}
