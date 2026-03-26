895df97c89e87d22fad78830211174c3ad951438
Pedro Alves <pedroalves.rj@icloud.com>
2026-03-26 12:25:33 -0300

    Criação de PessoaController com GET e POST

---
+++ ControleGastos/ControleGastos.Api/Controllers/PessoaController.cs
@@ -0,0 +1,27 @@
+﻿using ControleGastos.Api.Data;
+using ControleGastos.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleGastos.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController(AppDbContext context) : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pessoa>>> GetPessoas()
+        {
+            return await context.Pessoas.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Pessoa>> PostPessoa(Pessoa pessoa)
+        {
+            context.Pessoas.Add(pessoa);
+            await context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetPessoas), new { id = pessoa.Id }, pessoa);
+        }
+    }
+}
