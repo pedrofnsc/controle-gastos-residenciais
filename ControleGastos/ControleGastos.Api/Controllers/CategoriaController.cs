64584cde24d425fef3406808eff4ddd81d558921
Pedro Alves <pedroalves.rj@icloud.com>
2026-03-26 12:40:43 -0300

    Criação de CategoriaController com GET e POST

---
+++ ControleGastos/ControleGastos.Api/Controllers/CategoriaController.cs
@@ -0,0 +1,26 @@
+﻿using ControleGastos.Api.Data;
+using ControleGastos.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleGastos.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categoria>>> GetCategorias()
+        {
+            return await context.Categorias.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Categoria>> PostCategoria(Categoria categoria)
+        {
+            context.Categorias.Add(categoria);
+            await context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetCategorias), new { id = categoria.Id }, categoria);
+        }
+    }
+}
